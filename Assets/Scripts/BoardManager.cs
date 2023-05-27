1e68d2e9a836f1c191b2c2547678988e0c06e813
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2023-05-27 13:23:55 -0300

    Add player walk

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,15 +4,12 @@ using System.Collections.Generic;
 public class BoardManager : MonoBehaviour
 {
     private List<GameObject> tiles = new List<GameObject>();
+    private BoardSpawner boardSpawner;
 
     void Start()
     {
-        tiles = GetComponent<BoardSpawner>().SpawnBoard();
-    }
-
-    void Update()
-    {
-        // TestTilesByClicking();
+        boardSpawner = GetComponent<BoardSpawner>();
+        tiles = boardSpawner.SpawnBoard();
     }
 
     public int GetBoardSize()
@@ -25,74 +22,33 @@ public class BoardManager : MonoBehaviour
         return tiles[index].transform.position;
     }
 
-    public void TestTileByIndex(int index)
+    public int GetIndexOfTile(GameObject tile)
     {
-        TestTile(tiles[index]);
+        return tiles.IndexOf(tile);
     }
 
-    private void TestTilesByClicking()
+    public List<GameObject> GetAdjacentsByIndex(int index)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits = Physics.RaycastAll(ray);
-
-            foreach (RaycastHit hit in hits)
-                if (tiles.Contains(hit.collider.gameObject))
-                    TestTile(hit.collider.gameObject);
-        }
+        return GetAdjacents(tiles[index]);
     }
 
-    private void TestTile(GameObject tile)
-    {
-        tile.GetComponent<MeshRenderer>().material.color = Color.red;
-
-        List<GameObject> adjs = GetAdjacents(tile);
-        foreach (GameObject adj in adjs)
-            adj.GetComponent<MeshRenderer>().material.color = Color.blue;
-
-        List<GameObject> diags = GetDiagonals(tile);
-        foreach (GameObject diag in diags)
-            diag.GetComponent<MeshRenderer>().material.color = Color.green;
-    }
-
-    private List<GameObject> GetAdjacents(GameObject tile)
+    public List<GameObject> GetAdjacents(GameObject tile)
     {
         int index = tiles.IndexOf(tile);
-        int x = index % GetComponent<BoardSpawner>().baseSize;
-        int y = index / GetComponent<BoardSpawner>().baseSize;
+        int x = index % boardSpawner.baseSize;
+        int y = index / boardSpawner.baseSize;
 
         List<GameObject> adjs = new List<GameObject>();
 
         if (x > 0)
             adjs.Add(tiles[index - 1]);
-        if (x < GetComponent<BoardSpawner>().baseSize - 1)
+        if (x < boardSpawner.baseSize - 1)
             adjs.Add(tiles[index + 1]);
         if (y > 0)
-            adjs.Add(tiles[index - GetComponent<BoardSpawner>().baseSize]);
-        if (y < GetComponent<BoardSpawner>().baseSize - 1)
-            adjs.Add(tiles[index + GetComponent<BoardSpawner>().baseSize]);
+            adjs.Add(tiles[index - boardSpawner.baseSize]);
+        if (y < boardSpawner.baseSize - 1)
+            adjs.Add(tiles[index + boardSpawner.baseSize]);
 
         return adjs;
-    }
-
-    private List<GameObject> GetDiagonals(GameObject tile)
-    {
-        int index = tiles.IndexOf(tile);
-        int x = index % GetComponent<BoardSpawner>().baseSize;
-        int y = index / GetComponent<BoardSpawner>().baseSize;
-
-        List<GameObject> diags = new List<GameObject>();
-
-        if (x > 0 && y > 0)
-            diags.Add(tiles[index - GetComponent<BoardSpawner>().baseSize - 1]);
-        if (x < GetComponent<BoardSpawner>().baseSize - 1 && y > 0)
-            diags.Add(tiles[index - GetComponent<BoardSpawner>().baseSize + 1]);
-        if (x > 0 && y < GetComponent<BoardSpawner>().baseSize - 1)
-            diags.Add(tiles[index + GetComponent<BoardSpawner>().baseSize - 1]);
-        if (x < GetComponent<BoardSpawner>().baseSize - 1 && y < GetComponent<BoardSpawner>().baseSize - 1)
-            diags.Add(tiles[index + GetComponent<BoardSpawner>().baseSize + 1]);
-
-        return diags;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,44 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     public GameObject playerPrefab;
-    private BoardManager boardManager;
+
     private int boardSize;
-    private List<GameObject> players = new List<GameObject>();
+    private int currentPlayer = 0;
+    private int currentPlayerMoves = 3;
+    private BoardManager boardManager;
+
+    public List<int> playersIndex = new List<int>();
+    public List<GameObject> players = new List<GameObject>();    
+    private List<GameObject> canMoveTo = new List<GameObject>();
+    private List<GameObject> canAtackAt = new List<GameObject>();
 
     void Start()
     {
         boardManager = GameObject.Find("Board").GetComponent<BoardManager>();
         boardSize = boardManager.GetBoardSize();
+        SpawnPlayers();
+    }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (canMoveTo.Contains(hit.collider.gameObject))
+                    MovePlayerTo(hit.collider.gameObject);
+
+                else if (canAtackAt.Contains(hit.collider.gameObject))
+                    AtackPlayerAt(hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void SpawnPlayers()
+    {
         int randomIndex1 = Random.Range(0, boardSize);
         int randomIndex2 = Random.Range(0, boardSize);
 
@@ -21,11 +50,8 @@ public class GameManager : MonoBehaviour
 
         SpawnPlayerAt(randomIndex1);
         SpawnPlayerAt(randomIndex2);
-    }
 
-    void Update()
-    {
-        
+        UpdateMovePossibilities();
     }
 
     private bool BadSpawnCombination(int index1, int index2)
@@ -43,6 +69,102 @@ public class GameManager : MonoBehaviour
         position.y += 1f;
         GameObject player = Instantiate(playerPrefab, position, Quaternion.identity);
         players.Add(player);
-        boardManager.TestTileByIndex(index);
-    }    
+        playersIndex.Add(index);
+    }
+
+    private void UpdateMovePossibilities()
+    {
+        RemovePreviousColors();
+
+        UpdateCanMoveTo();
+        UpdateCanAtackAt();
+
+        foreach (GameObject tile in canAtackAt)
+            canMoveTo.Remove(tile);
+
+        AddCurrentColors();
+    }
+
+    private void RemovePreviousColors()
+    {
+        foreach (GameObject tile in canMoveTo)
+            tile.GetComponent<MeshRenderer>().material.color = Color.white;
+
+        foreach (GameObject tile in canAtackAt)
+            tile.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+
+    private void UpdateCanMoveTo()
+    {
+        canMoveTo = boardManager.GetAdjacentsByIndex(playersIndex[currentPlayer]);
+    }
+
+    private void UpdateCanAtackAt()
+    {
+        canAtackAt.Clear();
+
+        foreach (GameObject tile in canMoveTo)
+        {
+            foreach (int index in playersIndex)
+            {
+                if (boardManager.GetIndexOfTile(tile) == index)
+                    canAtackAt.Add(tile);
+            }
+        }
+    }
+
+    private void AddCurrentColors()
+    {
+        foreach (GameObject tile in canMoveTo)
+            tile.GetComponent<MeshRenderer>().material.color = Color.green;
+
+        foreach (GameObject tile in canAtackAt)
+            tile.GetComponent<MeshRenderer>().material.color = Color.red;
+    } 
+
+    private void PlayerMadeMove()
+    {
+        currentPlayerMoves--;
+
+        Debug.Log("Player " + currentPlayer + " made a move");
+        Debug.Log("Player " + currentPlayer + " has " + currentPlayerMoves + " moves left");
+
+        if (currentPlayerMoves <= 0)
+            NextPlayer();
+
+        UpdateMovePossibilities();
+    }
+
+    private void NextPlayer()
+    {
+        currentPlayer++;
+
+        if (currentPlayer >= players.Count)
+            currentPlayer = 0;
+
+        currentPlayerMoves = 3;
+    }
+
+    private void MovePlayerTo(GameObject tile)
+    {
+        players[currentPlayer].transform.position = tile.transform.position;
+        playersIndex[currentPlayer] = boardManager.GetIndexOfTile(tile);
+        PlayerMadeMove();
+    }
+
+    private void AtackPlayerAt(GameObject tile)
+    {
+        int index = boardManager.GetIndexOfTile(tile);
+        int playerIndex = playersIndex.IndexOf(index);
+
+        Destroy(players[playerIndex]);
+
+        players.RemoveAt(playerIndex);
+        playersIndex.RemoveAt(playerIndex);
+
+        if (playerIndex < currentPlayer)
+            currentPlayer--;
+
+        PlayerMadeMove();
+    }
 }
